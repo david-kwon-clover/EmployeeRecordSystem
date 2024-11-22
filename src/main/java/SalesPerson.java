d5567b7c6afb6ad7209ade321545a0c64fa3c029
David Kwon <david.kwon@clover.com>
2024-11-22 13:50:29 -0700

    Implement Salesperson constructor and getter/setter methods

--- src/main/java/SalesPerson.java
+++ src/main/java/SalesPerson.java
@@ -1,2 +1,16 @@
-public class SalesPerson {
+public class SalesPerson extends Employee {
+  private int salesTarget;
+
+  public SalesPerson(int employeeId, String name, int age, String jobTitle, int salary, int salesTarget) {
+    super(employeeId, name, age, jobTitle, salary);
+    this.salesTarget = salesTarget;
+  }
+  
+  public int getSalesTarget() {
+    return this.salesTarget;
+  }
+
+  public void setSalesTarget(int salesTarget) {
+    this.salesTarget = salesTarget;
+  }
 }
