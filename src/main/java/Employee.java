cadcf327b0c7f69015a62d4f3311cdf8380d128f
David Kwon <david.kwon@clover.com>
2024-11-22 13:25:13 -0700

    Implement Employee class constructor

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,3 +1,15 @@
 public class Employee {
+  int employeeId;
+  String name;
+  int age;
+  String jobTitle;
+  int salary;
 
+  Employee(int employeeId, String name, int age, String jobTitle, int salary) {
+    this.employeeId = employeeId;
+    this.name = name;
+    this.age = age;
+    this.jobTitle = jobTitle;
+    this.salary = salary;
+  }
 }
