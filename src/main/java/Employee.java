4126a0bd9f5795ebfa93f9a4002d4ca9a8b906cf
David Kwon <david.kwon@clover.com>
2024-11-22 13:31:22 -0700

    Add getter and setters for all Employee properties

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,15 +1,55 @@
 public class Employee {
-  int employeeId;
-  String name;
-  int age;
-  String jobTitle;
-  int salary;
+  private int employeeId;
+  private String name;
+  private int age;
+  private String jobTitle;
+  private int salary;
 
   Employee(int employeeId, String name, int age, String jobTitle, int salary) {
     this.employeeId = employeeId;
     this.name = name;
     this.age = age;
     this.jobTitle = jobTitle;
+    this.salary = salary;
+  }
+
+  public int getEmployeeId() {
+    return this.employeeId;
+  }
+
+  public void setEmployeeId(int employeeId) {
+    this.employeeId = employeeId;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return this.age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getJobTitle() {
+    return this.jobTitle;
+  }
+
+  public void setJobTitle(String jobTitle) {
+    this.jobTitle = jobTitle;
+  }
+
+  public int getSalary() {
+    return this.salary;
+  }
+
+  public void setSalary(int salary) {
     this.salary = salary;
   }
 }
--- src/main/java/Manager.java
+++ src/main/java/Manager.java
@@ -1,2 +1,6 @@
-public class Manager {
+public class Manager extends Employee {
+  String department;
+  Manager() {
+    super()
+  }
 }
